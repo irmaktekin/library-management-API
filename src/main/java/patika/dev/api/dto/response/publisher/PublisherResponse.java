fd6714cbf24a9f8f32dbec9fa47880c290bf54a3
irmaktekin <irmaktekin97@gmail.com>
2024-07-16 19:06:27 +0300

    dto for publisher response is added.

---
+++ src/main/java/patika/dev/api/dto/response/publisher/PublisherResponse.java
@@ -0,0 +1,13 @@
+package patika.dev.api.dto.response.publisher;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PublisherResponse {
+    private int id;
+    private String name;
+}
