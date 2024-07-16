a7c1b2ff94857eb274834a233edd5773441b4126
irmaktekin <irmaktekin97@gmail.com>
2024-07-16 19:00:18 +0300

    Category entity is created.

---
+++ src/main/java/patika/dev/api/entities/Category.java
@@ -0,0 +1,51 @@
+package patika.dev.api.entities;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Entity
+@Table(name="category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private int id;
+
+    @ManyToMany(mappedBy = "categoryList")
+    private List<Book> books;
+
+    @Column(name = "category_name",length = 100, nullable = false)
+    private String categoryName;
+
+    @Column(name = "category_description",length = 300, nullable = false)
+    private String categoryDescription;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryDescription() {
+        return categoryDescription;
+    }
+
+    public void setCategoryDescription(String categoryDescription) {
+        this.categoryDescription = categoryDescription;
+    }
+
+    public Category() {
+    }
+}
